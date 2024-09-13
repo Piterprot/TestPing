76ff53969d35152221839491a4e29f9436ae4033
Pietro <79109577+Piterprot@users.noreply.github.com>
2024-09-13 12:00:44 +0200

    Improved Logic, added some image

--- ProvaPing/ProvaPing/MainWindow.xaml.cs
+++ ProvaPing/ProvaPing/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Net.NetworkInformation;
+using System.Runtime.Intrinsics.X86;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace provaPing
 {
@@ -45,9 +47,30 @@ namespace provaPing
                 string millisec = reply.RoundtripTime.ToString();
                 int millisecToInt = int.Parse(millisec);
                 txtStatus.Text = status;
+
+
+
+
+
+
+
+
+
+
+
                 if (status == "Success")
                 {
                     txtStatus.Foreground = Brushes.Green;
+
+                    BitmapImage bi3 = new BitmapImage();
+                    bi3.BeginInit();
+                    bi3.UriSource = new Uri("success.png", UriKind.Relative);
+                    bi3.EndInit();
+                    StatIcon.Stretch = Stretch.Fill;
+                    StatIcon.Source = bi3;
+
+
+
                 }
                 else if (status == "TimedOut")
                 {
@@ -56,17 +79,32 @@ namespace provaPing
                 else if (millisecToInt > 50)
                 {
                     txtStatus.Foreground = Brushes.Yellow;
+                    BitmapImage bi2 = new BitmapImage();
+                    bi2.BeginInit();
+                    bi2.UriSource = new Uri("warning.png", UriKind.Relative);
+                    bi2.EndInit();
+                    StatIcon.Source = bi2;
+
+
+
                 }
                 else
                 {
                     txtStatus.Foreground = Brushes.Red;
+
+                    BitmapImage bi1 = new BitmapImage();
+                    bi1.BeginInit();
+                    bi1.UriSource = new Uri("error.png", UriKind.Relative);
+                    bi1.EndInit();
+                    StatIcon.Source = bi1;
+
                 }
                 txtMs.Text = millisec + " ms";
                 this.Cursor = Cursors.Arrow;
             }
-            catch
+            catch(System.Net.NetworkInformation.PingException)
             {
-                MessageBox.Show("Inserire un indirizzo IP valido");
+                MessageBox.Show("L'indirizzo IP non risulta essere valido");
                 return;
             }
         }
